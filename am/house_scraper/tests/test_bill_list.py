1d800ff3a3acaf5843a09ddba6a39521a4a2ddc6
James Gordon <gordon.je@gmail.com>
2017-05-05 16:03:46 -0500

    Remove BadMarkup test

--- am/house_scraper/tests/test_bill_list.py
+++ am/house_scraper/tests/test_bill_list.py
@@ -44,36 +44,3 @@ class BillListScraperTest(TestCase):
             len(self.bill_list.bill_urls),
             1389,
         )
-
-
-class BillListScraperBadMarkupTest(TestCase):
-    """
-    Test that an exception is raised when expected tags not found in markup.
-    """
-
-    @classmethod
-    def setUp(self):
-        """
-        Set up test case.
-        """
-        sample_markup_path = os.path.join(
-            settings.BASE_DIR,
-            'house_scraper',
-            'tests/sample_markup/index.aspx'
-        )
-        with open(sample_markup_path, 'rb') as f:
-            content = f.read()
-
-        # Mock any GET request
-        adapter = requests_mock.Adapter()
-        adapter.register_uri('GET', requests_mock.ANY, content=content)
-        with requests.Session() as session:
-            session.mount('http://house.mo.gov', adapter)
-            self.bill_list = BillListScraper(session=session)
-
-    def test_bill_list_not_found(self):
-        """
-        Test that an exception is raised when tag containing bill urls not found.
-        """
-        self.bill_list.bill_urls
-        self.assertRaises(Exception)
