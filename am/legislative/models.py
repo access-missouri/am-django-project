06cf8d74c6a74ac51b838a1e5d1bb262b01f6136
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2017-11-13 13:17:42 -0600

    Vote models and import code.

--- am/legislative/models.py
+++ am/legislative/models.py
@@ -9,6 +9,7 @@ from general.models import (
     AMBaseModel,
     Organization,
     Membership,
+    Person,
 )
 from django.utils.encoding import python_2_unicode_compatible
 
@@ -302,4 +303,81 @@ class BillSponsorship(AMBaseModel):
         return '{} sponsorship of {}'.format(
             self.member.person,
             self.bill,
+        )
+
+
+@python_2_unicode_compatible
+class BillVote(AMBaseModel):
+    """
+    The full question context of a vote on a bill.
+    """
+
+    bill = models.ForeignKey(
+        Bill,
+        related_name='votes',
+        help_text='Reference to the bill being voted on.',
+    )
+    issue = models.CharField(
+        max_length=512,
+        help_text='What the vote was on - as described in journal.',
+    )
+    yes = models.IntegerField(
+        default=0
+    )
+    no = models.IntegerField(
+        default=0
+    )
+    did_not_vote = models.IntegerField(
+        default=0
+    )
+    absent = models.IntegerField(
+        default=0
+    )
+    did_pass = models.BooleanField(
+        default=False
+    )
+    date = models.DateField(
+        help_text="Date the vote was taken."
+    )
+
+    def __str__(self):
+        return '{} on {}'.format(
+            self.issue,
+            self.bill,
+        )
+
+    class Meta:
+        """
+        Model options.
+        """
+
+        ordering = ['-date',]
+
+
+@python_2_unicode_compatible
+class PersonVote(AMBaseModel):
+    """
+    A vote on a bill by a person.
+    """
+
+    bill_vote = models.ForeignKey(
+        BillVote,
+        related_name='votes',
+        help_text='Reference to the broad vote in question.',
+    )
+    person = models.ForeignKey(
+        Person,
+        related_name='votes',
+        help_text='Reference to the person casting the vote.',
+    )
+    opinion = models.CharField(
+        max_length=12,
+        help_text='What the person actually voted.',
+    )
+
+    def __str__(self):
+        return '{} vote of {}: {}'.format(
+            self.person,
+            self.bill_vote,
+            self.opinion
         )
---
+++ am/ls_importer/management/commands/import_votes.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Import a folder (~/votes) full of vote JSON from Legiscan to the database.
+"""
+from django.core.management.base import BaseCommand
+from legislative.models import Bill, BillVote, PersonVote
+from ls_importer.models import LSIDPerson, LSIDBill
+import json
+from datetime import datetime
+import os
+from tqdm import tqdm
+
+
+class Command(BaseCommand):
+    """
+    Import a folder (~/votes) full of vote JSON from Legiscan to the database.
+    """
+
+    help = 'Import a folder full of vote JSON from Legiscan to the database.'
+
+    def handle(self, *args, **options):
+        """
+        Make it happen.
+        """
+
+        def json_to_vote(json_path):
+            json_data = open(json_path)
+            vote_json = json.load(json_data)['roll_call']
+
+
+            vote_ls_id = vote_json['roll_call_id']
+            bill_ls_id = vote_json['bill_id']
+            vote_date = datetime.strptime(vote_json['date'], "%Y-%m-%d")
+            vote_issue = vote_json['desc']
+            vote_yes = vote_json['yea']
+            vote_no = vote_json['nay']
+            vote_nv = vote_json['nv']
+            vote_absent = vote_json['absent']
+            vote_passed = True if vote_json['passed'] > 0 else False
+
+            vote_opinions = vote_json['votes']
+
+            vote_bill = LSIDBill.objects.get(
+                lsid=bill_ls_id
+            ).bill
+
+            broad_vote, broad_vote_created = BillVote.objects.get_or_create(
+                bill=vote_bill,
+                date=vote_date,
+                issue=vote_issue,
+                defaults={
+                    'yes': vote_yes,
+                    'no': vote_no,
+                    'did_not_vote': vote_nv,
+                    'absent': vote_absent,
+                    'did_pass': vote_passed,
+                }
+            )
+
+            for opinion in vote_opinions:
+                person = LSIDPerson.objects.get(
+                    lsid=opinion['people_id'],
+                ).person
+
+                opinion_mod, op_mod_created = PersonVote.objects.get_or_create(
+                    person=person,
+                    bill_vote=broad_vote,
+                    opinion=opinion['vote_text']
+                )
+
+        target_directory = os.path.join(os.path.expanduser("~"), 'votes')
+        for file in tqdm(os.listdir(target_directory)):
+            if file.endswith(".json"):
+                json_to_vote(os.path.join(target_directory, file))
