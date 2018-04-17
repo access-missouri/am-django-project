29b976fb2771877cd1530a27e5db1d96242d97d6
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2018-04-17 09:07:55 -0500

    Integrating model bill detector into main project.

--- am/am/settings.py
+++ am/am/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'search',
     'api',
     'scraper',
+    'modelbills',
 ]
 
 MIDDLEWARE = [
---
+++ am/modelbills/models.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from general.models import AMBaseModel, Organization
+
+# Create your models here.
+class ModelBill(AMBaseModel):
+    name = models.CharField(max_length=512,
+                            blank=False,
+                            null=False)
+    origin = models.ForeignKey(Organization,
+                               related_name="model_bills")
+    text = models.TextField()
+    source_url = models.URLField(max_length=512,
+                                 blank=True,
+                                 null=True)
+
+    def get_absolute_url(self):
+        return '/modelbills/m/{}'.format(
+            self.id,
+        )
