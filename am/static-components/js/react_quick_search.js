214e38f0325588f2c1c3cc856410b157fe053335
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2018-05-14 12:10:41 -0500

    Added finance to quick search.

--- am/static-components/js/react_quick_search.js
+++ am/static-components/js/react_quick_search.js
@@ -37,6 +37,15 @@ class QuickSearchResult extends React.Component {
                     </span>
                 )
             }
+            {
+                (this.props.result.type == 'finance') &&
+                (
+                    <span>
+                        <h3 className="type-finance">Finance Entity</h3>
+                        <h3 className="label"><a href={this.props.result.url}>{this.props.result.name}</a></h3>
+                    </span>
+                )
+            }
         </div>);
     }
 }
@@ -77,6 +86,8 @@ class AMQuickSearch extends React.Component {
 
         let billRequestUrl = (`/api/bills/?identifier_search=${encodeURI(query)}`);
         let personRequestUrl = (`/api/people/?index_name_search=${encodeURI(query)}`);
+        let financeRequestUrl = (`/api/financeentities/?name_search=${encodeURI(query)}`);
+
 
 
         // Bill Fetch
@@ -134,6 +145,34 @@ class AMQuickSearch extends React.Component {
 
             this.setState({
                 searchResults: this.state.searchResults.concat(billResults),
+                searchReturned: true
+            });
+        });
+
+        // Finance Fetch
+        fetch(financeRequestUrl,{
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then((response) => {
+            let jsonResp = response.json();
+            return jsonResp;
+        }, function(error) {
+            // handle network error
+        }).then((data) => {
+
+            let financeResults = data['results'].slice(0,4).map(
+                function(result){
+                    return {
+                        type: 'finance',
+                        name: result.name,
+                        url: `/finance/entities/${result.id}`
+                    }
+                }
+            );
+
+            this.setState({
+                searchResults: this.state.searchResults.concat(financeResults),
                 searchReturned: true
             });
         });
