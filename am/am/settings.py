d2c6bd4cc578abdfc3a727cbd605606b8e39139f
Nathan Lawrence <nathan@nathanlawrence.org>
2018-10-31 09:27:16 -0500

    Index contest model, add elections app to project run

--- am/am/settings.py
+++ am/am/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'scraper',
     'modelbills',
     'tags',
+    'elections',
 ]
 
 MIDDLEWARE = [
--- am/elections/models/__init__.py
+++ am/elections/models/__init__.py
@@ -5,7 +5,9 @@ Access Missouri models related to elections.
 """
 
 from .election import Election
+from .contest import Contest
 
 __all__ = (
     'Election',
+    'Contest'
 )
