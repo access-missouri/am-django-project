196fa946b1bd2bc1fa8985aea535643b310dbea0
Nathan Lawrence <nathan@nathanlawrence.org>
2019-01-05 20:06:14 -0600

    Added house bill page content interpreter.

--- am/scraper/interpreters/HouseBillPageContentInterpreter.py
+++ am/scraper/interpreters/HouseBillPageContentInterpreter.py
@@ -3,12 +3,55 @@
 """
 Scraper for requesting, caching and parsing house.mo.gov lists.
 """
-from scraper.interpreters import BaseInterpreter
-
+from scraper.interpreters import BaseInterpreter, HouseBillPageActionsInterpreter
+from scraper.url_helpers import house_url
 
 class HouseBillPageContentInterpreter(BaseInterpreter):
 
     def __init__(self, url):
+        self._attributes = None
+        self._actions_interpreter = None
+        self._actions = None
         super(HouseBillPageContentInterpreter, self).__init__(
             url=url)
 
+    @property
+    def attributes(self):
+        if not self._attributes:
+            attributes_rows = (self.soup
+                               .find("div", id="BillJunk")
+                               .find("table")
+                               .find("tbody")
+                               .find_all("tr"))
+            attributes = {}
+            for row in attributes_rows:
+                # Skip over any rows that don't have labels.
+                if row.find("th"):
+                    attributes.update([(
+                        row.find("th").get_text(),
+                        row.find("td").get_text()
+                    )])
+            self._attributes = attributes
+
+        return self._attributes
+
+    @property
+    def bill_actions_page_url(self):
+        links = self.soup.find_all("a")
+        for link in links:
+            if "BillActions" in link["href"]:
+                return link["href"]
+        return None
+
+    @property
+    def actions_interpreter(self):
+        if not self._actions_interpreter:
+            self._actions_interpreter = HouseBillPageActionsInterpreter(
+                house_url(self.bill_actions_page_url))
+        return self._actions_interpreter
+
+    @property
+    def actions(self):
+        if not self._actions:
+            self._actions = self.actions_interpreter.actions
+        return self._actions
---
+++ am/scraper/tests/test_integration_house_bill_content.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Integration tests around House Bill List scraping.
+"""
+
+from django.test import TestCase
+
+from scraper.interpreters import HouseBillPageContentInterpreter
+
+
+class HouseBillContentInterpreterTestCase(TestCase):
+
+    def setUp(self):
+        self.interpreter = HouseBillPageContentInterpreter(
+            url="https://raw.githubusercontent.com/access-missouri/am-scraper-test-mirror/master/house/bills/20190105-hb-26-bill-content.html") #noqa
+
+    def test_attributes_dictionary_captured(self):
+        self.assertEqual(len(self.interpreter.attributes), 7)
+
+    def test_access_actions(self):
+        self.assertGreater(len(self.interpreter.actions), 0)
