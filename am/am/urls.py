a32f688c5287c77f0aba9d1b0d8f88e299c31823
Nathan Lawrence <nathan@nathanlawrence.org>
2019-05-04 11:34:39 -0500

    Disaled CSRF for GQL endpoint, which uses JWT anyway.

--- am/am/urls.py
+++ am/am/urls.py
@@ -12,6 +12,7 @@ from legislative.views import BillDetailView, VoteDetailView, BillVotesListView,
 from django.conf.urls.static import static
 from django.conf.urls import handler404, handler500
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
 
@@ -20,7 +21,7 @@ from search.views import BillSearchListView, PersonSearchListView, FinanceSearch
 urlpatterns = [
                   url(r'^admin/', admin.site.urls),
                   url(r'^$', HomePageView.as_view()),
-                  url(r'^graphql', GraphQLView.as_view(graphiql=True)),
+                  url(r'^graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
                   url(r'^bills/$', BillsHomeView.as_view()),
                   url(r'^people/$', PeopleHomeView.as_view()),
                   url(r'^bills/(?P<id>[\w-]+)/$', BillDetailView.as_view(), name='bill'),
